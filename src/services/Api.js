43f28f128325236decc06ecb6c2ad73d2f4f287d
jonnylawrence-macbook <jonnylawrence@jonnylawrence-RetinaMacBook.local>
2019-09-01 20:27:18 +0100

    API

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://localhost:3000/`
+  baseURL: `https://b2c-requestchecker.herokuapp.com/register/`
 });
 
 export default instance;
