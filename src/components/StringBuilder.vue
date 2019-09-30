7ce37cb3deab9c1d205a21ef7f02fc0efb92bf75
Jonnylawrence <jonnylawrence@yahoo.co.uk>
2019-09-30 14:27:37 +0100

    addedvuetify

--- src/components/StringBuilder.vue
+++ src/components/StringBuilder.vue
@@ -3,86 +3,88 @@
 		<!-- Check that the SDK client is not currently loading before accessing is methods -->
 		<div v-if="!$auth.loading">
 			<!-- show login when not authenticated -->
-			<button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+			<v-btn v-if="!$auth.isAuthenticated" @click="login">Log in</v-btn>
 			<!-- show logout when authenticated -->
-			<button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+			<v-btn v-if="$auth.isAuthenticated" @click="logout">Log out</v-btn>
 		</div>
 
-		<div class="container px-lg-5" v-if="$auth.isAuthenticated">
-			<jonheader></jonheader>
-			<div class="row">
-				<div class="col-6">
-					<v-radio-group v-model="radiosTenant" label="Tenant" row>
-						<v-radio
-							name="radiosTenant"
-							value="https://uat-account.np.bupaglobal.com/neubgdat01atluat01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-uat-signinsignup/oauth2/v2.0/authorize"
-							key="UAT"
-							label="UAT"
-						/>
-						<v-radio
-							name="radiosTenant"
-							value="https://nft-account.np.bupaglobal.com/neubgdat01atlnft01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-nft-signinsignup/oauth2/v2.0/authorize"
-							key="NFT"
-							label="NFT"
-						/>
-					</v-radio-group>
-					<v-radio-group
-						v-model="radiosLoA"
-						label="Authentication Level"
-						row
-						hint="Different authentication workflows will be executed by the B2C based on this Level of Access"
-					>
-						<v-radio
-							name="radiosLoA"
-							value="L1"
-							key="L1"
-							label="L1"
+		<div v-if="$auth.isAuthenticated">
+			<div class="container px-lg-5">
+				<jonheader></jonheader>
+				<div class="row">
+					<div class="col-6">
+						<v-radio-group v-model="radiosTenant" label="Tenant" row>
+							<v-radio
+								name="radiosTenant"
+								value="https://uat-account.np.bupaglobal.com/neubgdat01atluat01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-uat-signinsignup/oauth2/v2.0/authorize"
+								key="UAT"
+								label="UAT"
+							/>
+							<v-radio
+								name="radiosTenant"
+								value="https://nft-account.np.bupaglobal.com/neubgdat01atlnft01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-nft-signinsignup/oauth2/v2.0/authorize"
+								key="NFT"
+								label="NFT"
+							/>
+						</v-radio-group>
+						<v-radio-group
+							v-model="radiosLoA"
+							label="Authentication Level"
+							row
 							hint="Different authentication workflows will be executed by the B2C based on this Level of Access"
-						/>
-						<v-radio name="radiosLoA" value="L2" key="L2" label="L2" />
-						<v-radio name="radiosLoA" value="L3" key="L3" label="L3" />
-					</v-radio-group>
-					<v-text-field v-model="token_length" label="Token Length [days]" required></v-text-field>
-					<v-text-field v-model="appid" label="Client ID/App ID" required></v-text-field>
+						>
+							<v-radio
+								name="radiosLoA"
+								value="L1"
+								key="L1"
+								label="L1"
+								hint="Different authentication workflows will be executed by the B2C based on this Level of Access"
+							/>
+							<v-radio name="radiosLoA" value="L2" key="L2" label="L2" />
+							<v-radio name="radiosLoA" value="L3" key="L3" label="L3" />
+						</v-radio-group>
+						<v-text-field v-model="token_length" label="Token Length [days]" required></v-text-field>
+						<v-text-field v-model="appid" label="Client ID/App ID" required></v-text-field>
 
-					<ValidationObserver v-slot="{ valid, passes  }">
-						<form @encodeJWT.prevent="passes(encodeJWT)">
-							<ValidationProvider rules="required">
-								<v-text-field v-model="redirect_uri" label="Redirect URI (your site)" required></v-text-field>
-							</ValidationProvider>
-							<ValidationProvider rules="required">
-								<v-text-field v-model="client_secret" label="Client Secret" required></v-text-field>
-							</ValidationProvider>
+						<ValidationObserver v-slot="{ valid, passes  }">
+							<form @encodeJWT.prevent="passes(encodeJWT)">
+								<ValidationProvider rules="required">
+									<v-text-field v-model="redirect_uri" label="Redirect URI (your site)" required></v-text-field>
+								</ValidationProvider>
+								<ValidationProvider rules="required">
+									<v-text-field v-model="client_secret" label="Client Secret" required></v-text-field>
+								</ValidationProvider>
 
-							<v-btn :disabled="!valid" v-on:click="encodeJWT" elevation="10" rounded>Encode JWT</v-btn>
-							<br />
-						</form>
-					</ValidationObserver>
-					<br />
+								<v-btn :disabled="!valid" v-on:click="encodeJWT" elevation="10" rounded>Encode JWT</v-btn>
+								<br />
+							</form>
+						</ValidationObserver>
+						<br />
 
-					<v-radio-group v-model="radiosLocale" label="Locale" row>
-						<v-radio name="radiosLocale" value="en-GB" key="en-GB" label="en-GB" />
-						<v-radio name="radiosLocale" value="zh-ZK" key="zh-ZK" label="zh-ZK" />
-					</v-radio-group>
+						<v-radio-group v-model="radiosLocale" label="Locale" row>
+							<v-radio name="radiosLocale" value="en-GB" key="en-GB" label="en-GB" />
+							<v-radio name="radiosLocale" value="zh-ZK" key="zh-ZK" label="zh-ZK" />
+						</v-radio-group>
 
-					<v-radio-group v-model="radiosBranding" label="Branding" row>
-						<v-radio name="radiosLocale" value="europe" key="europe" label="Europe" />
-						<v-radio name="radiosLocale" value="hongkong" key="hongkong" label="Hong Kong" />
-						<v-radio name="radiosLocale" value="jontestbrand" key="jontestbrand" label="Test" />
-					</v-radio-group>
+						<v-radio-group v-model="radiosBranding" label="Branding" row>
+							<v-radio name="radiosLocale" value="europe" key="europe" label="Europe" />
+							<v-radio name="radiosLocale" value="hongkong" key="hongkong" label="Hong Kong" />
+							<v-radio name="radiosLocale" value="jontestbrand" key="jontestbrand" label="Test" />
+						</v-radio-group>
 
-					<v-textarea v-model="textbox" full-width outlined cols="60" auto-grow></v-textarea>
-					<br />
+						<v-textarea v-model="textbox" full-width outlined cols="60" auto-grow></v-textarea>
+						<br />
+					</div>
+					<div class="col-6">
+						<v-btn v-on:click="randomstring" elevation="10" rounded>Generate Nonce/State</v-btn>
+						<br />
+						<v-textarea v-model="urlrequest" outlined cols="60" auto-grow></v-textarea>
+						<v-btn v-on:click="connectb2c" elevation="10" rounded>Connect</v-btn>
+					</div>
 				</div>
-				<div class="col-6">
-					<v-btn v-on:click="randomstring" elevation="10" rounded>Generate Nonce/State</v-btn>
-					<br />
-					<v-textarea v-model="urlrequest" outlined cols="60" auto-grow></v-textarea>
-					<v-btn v-on:click="connectb2c" elevation="10" rounded>Connect</v-btn>
-				</div>
+
+				<br />
 			</div>
-
-			<br />
 		</div>
 	</v-app>
 </template>
